72dcbbc75b3927e49be68af4665effbf463c4d39
Nosindiso13 <nosindisomoyo96@gmail.com>
2026-04-04 21:12:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ from datetime import datetime, timedelta
 from passlib.context import CryptContext
 
 # Database imports
-from sqlalchemy import create_engine, Column, Integer, String, Boolean
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
