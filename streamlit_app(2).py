29a4d8c828d64f9180fbd1f36062c2ddc90ffe8d
Nosindiso13 <nosindisomoyo96@gmail.com>
2026-04-05 19:06:53 +0200

    Update streamlit_app(2).py

--- streamlit_app(2).py
+++ streamlit_app(2).py
@@ -23,7 +23,7 @@ MARKET_PATH = 'market_trends.csv'
 
 # Database Setup (local to Streamlit now)
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
