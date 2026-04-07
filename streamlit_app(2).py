d6b4538ae9af4f81af359f4c16d57794455f9cb5
Nosindiso13 <nosindisomoyo96@gmail.com>
2026-04-07 02:51:24 +0200

    Update streamlit_app(2).py

--- streamlit_app(2).py
+++ streamlit_app(2).py
@@ -187,7 +187,7 @@ with tabs[1]:
         
 
         img = Image.open(uploaded_file).resize((224, 224))
-        x = image.img_to_array(img)
+        
         x = np.expand_dims(x, axis=0)
         x = preprocess_input(x)
 
