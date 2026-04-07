86380270ec4bac8273a4876e22f5d9ac61a29ae2
Nosindiso13 <nosindisomoyo96@gmail.com>
2026-04-07 02:52:10 +0200

    Update streamlit_app(2).py

--- streamlit_app(2).py
+++ streamlit_app(2).py
@@ -188,8 +188,7 @@ with tabs[1]:
 
         img = Image.open(uploaded_file).resize((224, 224))
         
-        x = np.expand_dims(x, axis=0)
-        x = preprocess_input(x)
+        
 
         preds = pest_model.predict(x)
         results = decode_predictions(preds, top=3)[0]
