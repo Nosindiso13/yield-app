d1525cfb6a3372b7b7119d1ead4e8c32656b2460
Nosindiso13 <nosindisomoyo96@gmail.com>
2026-04-08 21:07:39 +0200

    Update streamlit_app(2).py

--- streamlit_app(2).py
+++ streamlit_app(2).py
@@ -201,12 +201,12 @@ with tabs[0]:
        st.header("🛒 Farmer Marketplace")
       
     with st.form("market_form"):
-            seller_name = st.text_input('Name','contact')
-            crop_type = st.selectbox('Crop', ['Wheat', 'Maize', 'Potatoes', 'Rice, paddy', 'Sorghum', 'Soybeans'])
-            quantity = st.number_input('Quantity (kg)', min_value=1)
-            price = st.number_input('Asking Price (ZMW)', min_value=50)
-            if st.form_submit_button('Post Listing'):
-                st.success(f'Listing created for {seller_name}.')
+       seller_name = st.text_input('Name','contact')
+       crop_type = st.selectbox('Crop', ['Wheat', 'Maize', 'Potatoes', 'Rice, paddy', 'Sorghum', 'Soybeans'])
+       quantity = st.number_input('Quantity (kg)', min_value=1)
+       price = st.number_input('Asking Price (ZMW)', min_value=50)
+       if st.form_submit_button('Post Listing'):
+       st.success(f'Listing created for {seller_name}.')
 
     # LOGOUT
     if st.button("Logout"):
