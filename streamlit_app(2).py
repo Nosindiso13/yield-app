2b296eafda4f06170bda913567638a7de0327b2c
Nosindiso13 <nosindisomoyo96@gmail.com>
2026-04-08 20:54:35 +0200

    Update streamlit_app(2).py

--- streamlit_app(2).py
+++ streamlit_app(2).py
@@ -200,15 +200,7 @@ with tabs[0]:
     with tabs[4]:
        st.header("🛒 Farmer Marketplace")
       
-    col1, col2 = st.columns(2)
-   
-     with st.subheader('Trending Crops This Season')
-        if os.path.exists("market trends"):
-            df_trends = pd.read_csv("market trends")
-            st.dataframe(df_trends, use_container_width=True)
-        else: st.info('No trend data available.')
-    
-       with st.subheader('List Your Crop for Sale')
+     with st.subheader('List Your Crop for Sale')
         with st.form('market_form'):
             seller_name = st.text_input('Name','contact')
             crop_type = st.selectbox('Crop', ['Wheat', 'Maize', 'Potatoes', 'Rice, paddy', 'Sorghum', 'Soybeans'])
