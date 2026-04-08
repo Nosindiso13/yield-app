e44e96a615a8f0dca7aa4acb2a408afef438dd32
Nosindiso13 <nosindisomoyo96@gmail.com>
2026-04-08 18:56:38 +0200

    Update streamlit_app(2).py

--- streamlit_app(2).py
+++ streamlit_app(2).py
@@ -14,9 +14,6 @@ from PIL import Image
 # ==============================
 DATABASE_URL = "sqlite:///users.db"
 
-SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
-
 class User(Base):
     _tablename_ = "users"
     id = Column(Integer, primary_key=True)
